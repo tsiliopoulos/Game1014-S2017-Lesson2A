db67fef3fe3b3318847d6748dc4ba1a5d6b9e3f4
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-05-19 11:57:14 -0400

    added createjs test code

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,16 +1,32 @@
 // IIFE - Immediately Invoked Function Expression
 (function() {
+    let canvas = document.getElementById("canvas"); // hook into the canvas element
+    let stage;
+    let helloLabel;
 
     function Start() {
-        console.log("Game Started");
+       stage = new createjs.Stage(canvas); // create a stage container
+       createjs.Ticker.framerate = 60;
+       createjs.Ticker.on("tick", Update); // call Update every frame
+       Main();
     }
 
+    // Game Loop - triggers every frame
     function Update() {
+      helloLabel.rotation += 5;
 
+      stage.update(); // refresh the stage
     }
 
     function Main() {
+      console.log("Game Started");
 
+      helloLabel = new createjs.Text("Hello, World", "40px Consolas", "#000000");
+      helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+      helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+      helloLabel.x = 320;
+      helloLabel.y = 240;
+      stage.addChild(helloLabel);
     }
 
     window.onload = Start;
