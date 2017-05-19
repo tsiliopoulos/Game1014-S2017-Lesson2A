a5cb523855b9a01b2a1783bb2e410e9bc959bf8d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-05-19 11:38:22 -0400

    updated game.js

---
+++ Scripts/core/game.js
@@ -0,0 +1,18 @@
+// IIFE - Immediately Invoked Function Expression
+(function() {
+
+    function Start() {
+        console.log("Game Started");
+    }
+
+    function Update() {
+
+    }
+
+    function Main() {
+
+    }
+
+    window.onload = Start;
+})();
+
